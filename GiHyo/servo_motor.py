2404fa9f2f330c938ad2706017203129a99e5be0
Angihyo <agh367@naver.com>
2017-10-09 22:57:52 +0900

    서보모터 함수추가 갱신
    라즈베리파이 복구 위한 서보모터 부분 다시 작성

--- GiHyo/servo_motor.py
+++ GiHyo/servo_motor.py
@@ -10,6 +10,7 @@ servo_pwmpin = INI2.servo_pwm
 GPIO.setup(servo_pwmpin, GPIO.OUT)
 
 servo_motor = GPIO.PWM(servo_pwmpin, 50)
+GPIO.setwarnings(False)
 
 #3 ~ 5.4
 """left mode"""
@@ -21,7 +22,7 @@ def left_Drive():
 """right mode"""
 def right_Drive():
     try:
-        servo_motor.ChangeDutyCycle(5.4)
+        servo_motor.ChangeDutyCycle(5)
     except KeyboardInterrupt:
        servo_motor.stop()
 """forward mode"""
@@ -30,4 +31,18 @@ def forward_Drive():
         servo_motor.ChangeDutyCycle(4)
     except KeyboardInterrupt:
        servo_motor.stop()
-
+"""exclusive servo control"""
+def setting_Drive(servo_angle):
+    try:
+        if servo_angle==1:
+            right_Drive()
+            print("right")
+        elif servo_angle==-1:
+            left_Drive()
+            print("left")
+        else:
+            servo_motor.ChangeDutyCycle(4+servo_angle)
+            print("angle")
+            print(4+servo_angle)
+    except KeyboardInterrupt:
+        servo_motor.stop()
