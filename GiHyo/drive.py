7872831b228ee92d9d7d43c04f3754bf1fbb8795
Angihyo <agh367@naver.com>
2017-10-09 23:14:59 +0900

    주행 파일 갱신
    라즈베리파이 복구 위한 갱신

--- GiHyo/drive.py
+++ GiHyo/drive.py
@@ -4,6 +4,8 @@ import time
 import servo_motor as SERVO
 import initial as INI
 import arduinoserial as AS
+import picam as PICAM
+import servo_angle_calcul as SERVO_ANGLE
 
 """Setting"""
 GPIO.setmode(GPIO.BCM)
@@ -40,6 +42,8 @@ servo_motor = GPIO.PWM(servo_pwmpin, 50)
 SERVO.servo_motor.start(4)
 before_left=1
 before_right=1
+point = (0,0)
+before_point = (240,0)
 """motor stop"""
 def stop():
     pwmA_motor.ChangeDutyCycle(0)
@@ -64,20 +68,35 @@ def backward():
 
 """main"""
 while True:
+    try:
+        point = PICAM.camera_operating(before_point)
+        print(point)
+    except:
+        point = before_point
+    before_point = point
     left_input = GPIO.input(left_sensor_pin)
     right_input = GPIO.input(right_sensor_pin)
-    middle_input = AS.serial_control()
-    if before_left != left_input or before_right != right_input:
-        if left_input==0 and right_input==0:
-            SERVO.forward_Drive()
-            stop()
-        elif left_input==0:
-            SERVO.right_Drive()
-        elif right_input==0:
-            SERVO.left_Drive()
-        else:
-            SERVO.forward_Drive()
-    forward()
+    #middle_input = AS.serial_control()
+    middle_input = 30
+    print(left_input)
+    print(right_input)
+    if middle_input > 20:
+        forward()
+        if before_left == left_input or before_right == right_input:
+            if left_input==0 and right_input==0:
+                SERVO.forward_Drive()
+                #stop()
+                print('forward')
+            elif left_input==0:
+                SERVO.right_Drive()
+                print('right')
+            elif right_input==0:
+                SERVO.left_Drive()
+                print('left')
+            else:
+                SERVO.setting_Drive(SERVO_ANGLE.Servo_angle_calcul(point))
+    else:
+        stop()
     before_left = left_input
     before_right = right_input
 
