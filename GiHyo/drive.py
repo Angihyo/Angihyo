56c4ee35620c16ed74a42e26e4055a0bb253710f
Angihyo <agh367@naver.com>
2017-11-01 20:22:20 +0900

    Update drive.py
    드라이브 메인 옮김

--- GiHyo/drive.py
+++ GiHyo/drive.py
@@ -66,39 +66,3 @@ def backward():
     GPIO.output(in3_pin,True)
     GPIO.output(in4_pin,False)
 
-"""main"""
-while True:
-    try:
-        point = PICAM.camera_operating(before_point)
-        print(point)
-    except:
-        point = before_point
-    before_point = point
-    left_input = GPIO.input(left_sensor_pin)
-    right_input = GPIO.input(right_sensor_pin)
-    #middle_input = AS.serial_control()
-    middle_input = 30
-    print(left_input)
-    print(right_input)
-    if middle_input > 20:
-        forward()
-        if before_left == left_input or before_right == right_input:
-            if left_input==0 and right_input==0:
-                SERVO.forward_Drive()
-                #stop()
-                print('forward')
-            elif left_input==0:
-                SERVO.right_Drive()
-                print('right')
-            elif right_input==0:
-                SERVO.left_Drive()
-                print('left')
-            else:
-                SERVO.setting_Drive(SERVO_ANGLE.Servo_angle_calcul(point))
-    else:
-        stop()
-    before_left = left_input
-    before_right = right_input
-
-
-    
